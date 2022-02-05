f7e5282dc0f002c3d35572ff775fe5ad976c5327
Michael Fares <michaelfares@Michaels-MacBook-Pro.local>
2022-02-05 12:59:55 -0600

    still need to format earnings

--- src/components/EarningsLine.js
+++ src/components/EarningsLine.js
@@ -5,14 +5,14 @@ const EarningsLine = ({ data }) => {
 
     
     const medianEarningsData = [
-        { "Years after entry": "6", "Median Earnings": data["6_yrs_after_entry.median"] },
+        { "Years after entry": "6", "Median Earnings": data["6_yrs_after_entry"].median },
         { "Years after entry": "7", "Median Earnings": 1 },
         { "Years after entry": "8", "Median Earnings": 1 },
         { "Years after entry": "9", "Median Earnings": 1 },
-        { "Years after entry": "10", "Median Earnings": 1 },
+        { "Years after entry": "10", "Median Earnings": 1 }
     ]
     
-     console.log("median", medianEarningsData)
+     console.log("median", medianEarningsData, data)
 
     return (
        
